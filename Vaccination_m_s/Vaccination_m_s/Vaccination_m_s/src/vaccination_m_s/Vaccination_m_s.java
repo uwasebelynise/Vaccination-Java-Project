213134008eed1a0e9a0caf4568d5a3d3bdfc1399
uwasebelynise <117974292+uwasebelynise@users.noreply.github.com>
2023-01-25 12:58:11 -0800

    Vaccination Project
    This Is Java netbean Project

---
+++ Vaccination_m_s/Vaccination_m_s/Vaccination_m_s/src/vaccination_m_s/Vaccination_m_s.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vaccination_m_s;
+
+/**
+ *
+ * @author UR PC
+ */
+public class Vaccination_m_s {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
